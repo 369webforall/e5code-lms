14be21eb1a11ecd3209b2cb11ad96b330fcee0bb
369webforall <369webforall@gmail.com>
2024-05-05 09:12:40 +0545

    update

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,13 @@ import NextAuth from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import Resend from "next-auth/providers/resend";
 import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
+  theme: {
+    logo: "/logo.png",
+  },
   adapter: PrismaAdapter(prisma) as Adapter,
   callbacks: {
     session({ session, user }) {
@@ -18,8 +20,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google,
     GitHub,
-    Resend({
-      from: "no-reply@e5code.com",
-    }),
   ],
 });
