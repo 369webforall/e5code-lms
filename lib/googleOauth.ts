1945a124d965508529b7c84e1f386ac2b782381b
369webforall <369webforall@gmail.com>
2024-07-07 17:44:45 +0545

    update

--- lib/googleOauth.ts
+++ lib/googleOauth.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const googleOAuthClient = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  process.env.NEXT_PUBLIC_URL + "api/auth/google/callback"
+  process.env.NEXT_PUBLIC_URL + "/api/auth/google/callback"
 );
